b80c864fbc112efd1710728672fce1e62f0b3455
Juan Carlos "RMPixel <carlos@rmpcorreo.com>
2021-01-29 10:10:36 +0000

    Practica filtro fir terminada, arquitectura avanzada terminado todo

---
+++ ComposeStudioFiltroFir/JuanSiverio_DSP_2020/basico_main/Debug/configPkg/custom.mak
@@ -0,0 +1,5 @@
+## THIS IS A GENERATED FILE -- DO NOT EDIT
+.configuro: .libraries,64P linker.cmd package/cfg/hello_p64P.o64P
+
+linker.cmd: package/cfg/hello_p64P.xdl
+	$(SED) 's"^\"\(package/cfg/hello_p64Pcfg.cmd\)\"$""\"C:/git/ARQAVANZADA/ComposeStudioFiltroFir/basico_main/Debug/configPkg/\1\""' package/cfg/hello_p64P.xdl > $@
---
+++ ComposeStudioFiltroFir/JuanSiverio_DSP_2020/basico_main/Debug/utils.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+utils.obj: ../utils.c
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/cstdio
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/yvals.h
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/_lock.h
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h
+utils.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+
+../utils.c: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/cstdio: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
